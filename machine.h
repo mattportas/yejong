6ee28d6d3a65a014f4ad9f4454258b4eb5114364
Matt Portas <matt.r.portas@gmail.com>
2013-06-03 22:04:09 +0100

    Add name to add_job param

--- machine.h
+++ machine.h
@@ -11,7 +11,7 @@ class Machine
     public:
         Machine(const std::string& machine_name);
         const std::string& get_name() const;
-        void add_job(Job);
+        void add_job(Job new_job);
     private:
         std::string name;
         std::vector<Job> jobs;

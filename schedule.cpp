d833496f9f99794a3b4cf107f0f8a7ed81f9ea96
Andrew J. Bennieston <a.j.bennieston@gmail.com>
2013-06-03 19:01:29 +0100

    Update schedule.cpp to avoid ending up with local copies of data.
    The (old) variables first_job, second_job, first_machine, second_machine
    are _copied_ when inserted into the machine pool. Leaving local symbols
    for them in the main() scope could lead to them being manipulated later
    (as in line 18) but those manipulations would not affect the copies in
    the machine pool.
    
    The real issue should probably be solved by either
    [1] storing shared_ptr<Machine> and shared_ptr<Job> in the pool and
        allocating the objects through new
    - or -
    [2] adding a mechanism to the Pool class to obtain non-const references
        to the machines and jobs contained.

--- schedule.cpp
+++ schedule.cpp
@@ -7,16 +7,13 @@
 int main ()
 {
     Pool machine_pool;
-    Machine first_machine("one");
-    Machine second_machine("two");
     Job first_job(1);
-    Job second_job(2);
 
-    machine_pool.add_machine(first_machine);
-    machine_pool.add_machine(second_machine);
+    machine_pool.add_machine(Machine("one"));
+    machine_pool.add_machine(Machine("two"));
 
     machine_pool.add_job(first_job);
-    machine_pool.add_job(second_job);
+    machine_pool.add_job(Job(2));
 
     std::cout << "Job 1 moveable: " << first_job.moveable() << std::endl;
 
